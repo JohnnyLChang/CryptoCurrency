77d070de8e3b640fe67b71bd7c5c7e224b5f2ca7
Johnny Chang <riversnow@gmail.com>
2017-01-25 19:26:41 +0800

    implement basic blockchain function,Score 52

--- BlockChain/src/Block.java
+++ BlockChain/src/Block.java
@@ -15,9 +15,11 @@ public class Block {
 
     /** {@code address} is the address to which the coinbase transaction would go */
     public Block(byte[] prevHash, PublicKey address) {
+        System.out.println("create new block");
         prevBlockHash = prevHash;
         coinbase = new Transaction(COINBASE, address);
         txs = new ArrayList<Transaction>();
+        System.out.println("create new block completed");
     }
 
     public Transaction getCoinbase() {
